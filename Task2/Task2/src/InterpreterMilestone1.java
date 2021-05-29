299523583847b1968f0d35bd95dc5a3712869af9
Daniel <danielweissenborn@outlook.de>
2021-05-29 17:08:04 +0200

    One works almost completely only thing missing is structuring so it works without paramters and edge cases where you assign a variable multiple times.

--- Task2/Task2/src/InterpreterMilestone1.java
+++ Task2/Task2/src/InterpreterMilestone1.java
@@ -15,7 +15,7 @@ public class InterpreterMilestone1 {
 
      public static void helpermethod(){
        List<String> output = new ArrayList<>();
-        String code = "var foo; foo = 23; print(foo);";
+        String code = "var x; var y; var z; var q; z = 444;  print(z); y = z; print(y); q = y; print(q)";
         String regex = "[a-z][a-z]*\\s=\\s([a-z][a-z]*|[0-9][0-9]*);";
 
         final String string = code;
@@ -28,61 +28,22 @@ public class InterpreterMilestone1 {
         List<String> printStatements = new ArrayList<>();
         HashMap<String,String> pairsOfAssignment = new HashMap<>();
         while (matcher.find()) {
-            //System.out.println(matcher.group(0));
+            System.out.println(matcher.group(0));
+            System.out.println(matcher.group(1));
             assignments.add(matcher.group(0));
             for (int i = 1; i <= matcher.groupCount(); i++) {
                 //System.out.println("Value of assignment " + ": " + matcher.group(i));
                 String firstWord = matcher.group(0).replaceAll(" .*", "");
                 variableName.add(firstWord);
-                valueOfAssignment.add(matcher.group(i));
-            }
-        }
+                //System.out.println(firstWord);
+                    for(int j = 0; j<=variableName.size();j++){
+                        if(variableName.contains(matcher.group(1))) {//if the variable is already in the assignment list
+                        valueOfAssignment.add(valueOfAssignment.get(j));//somehow need value of this variable===)?
+                    }    else {
+                            valueOfAssignment.add(matcher.group(i));//else just assign the right value
+                        }
 
-        for(int i = 0; i<variableName.size();i++){
-            pairsOfAssignment.put(variableName.get(i),valueOfAssignment.get(i));
-        }
-        System.out.println("Assignments: " + assignments);
-        System.out.println("Name of variable: " + variableName);
-        System.out.println("Values of assignments: " + valueOfAssignment);
-        System.out.println("Assignmentpairs: " + pairsOfAssignment);
-
-        String regex2 = "[p][r][i][n][t][(]([a-z][a-z]*)[)]";
-        final Pattern pattern2 = Pattern.compile(regex2, Pattern.MULTILINE);
-        final Matcher matcher2 = pattern2.matcher(string);
-        while (matcher2.find()) {
-            //System.out.println(matcher2.group(0));
-            //System.out.println(matcher2.group(1));
-            printStatements.add(matcher2.group(1));
-        }
-        for(int i = 0; i<printStatements.size();i++){
-            for(String name: pairsOfAssignment.keySet())
-                if(printStatements.get(i).equals(name)){
-                    output.add(pairsOfAssignment.get(name));
                 }
-            }System.out.println(output);
-        }
-    public List<String> run() {
-        List<String> output = new ArrayList<>();
-        String code = "var x; var y; var z; z = 567; print(z); y = z; print(y);";
-        String regex = "[a-z][a-z]*\\s=\\s([a-z][a-z]*|[0-9][0-9]*);";
-
-        String string = code;
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        final Matcher matcher = pattern.matcher(string);
-
-        List<String> assignments = new ArrayList<>();
-        List<String> valueOfAssignment = new ArrayList<>();
-        List<String> variableName = new ArrayList<>();
-        List<String> printStatements = new ArrayList<>();
-        HashMap<String,String> pairsOfAssignment = new HashMap<>();
-        while (matcher.find()) {
-            //System.out.println(matcher.group(0));
-            assignments.add(matcher.group(0));
-            for (int i = 1; i <= matcher.groupCount(); i++) {
-                //System.out.println("Value of assignment " + ": " + matcher.group(i));
-                String firstWord = matcher.group(0).replaceAll(" .*", "");
-                variableName.add(firstWord);
-                valueOfAssignment.add(matcher.group(i));
             }
         }
 
@@ -107,11 +68,66 @@ public class InterpreterMilestone1 {
                 if(printStatements.get(i).equals(name)){
                     output.add(pairsOfAssignment.get(name));
                 }
+            }
+        System.out.println(output);
+        }
+    public List<String> run(String code) {//without parameter
+        List<String> output = new ArrayList<>();
+
+        String regex = "[a-z][a-z]*\\s=\\s([a-z][a-z]*|[0-9][0-9]*);";
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(code);
+
+        List<String> assignments = new ArrayList<>();
+        List<String> valueOfAssignment = new ArrayList<>();
+        List<String> variableName = new ArrayList<>();
+        List<String> printStatements = new ArrayList<>();
+        HashMap<String,String> pairsOfAssignment = new HashMap<>();
+        while (matcher.find()) {
+            //System.out.println(matcher.group(0));
+            assignments.add(matcher.group(0));
+                for (int i = 1; i <= matcher.groupCount(); i++) {
+                    //System.out.println("Value of assignment " + ": " + matcher.group(i));
+                    String firstWord = matcher.group(0).replaceAll(" .*", "");
+                    variableName.add(firstWord);
+                    //System.out.println(firstWord);
+                    if(variableName.contains(matcher.group(1))) {//if the variable is already in the assignment list
+                        valueOfAssignment.add(valueOfAssignment.get(0));//somehow need value of this variable===)?
+                    }
+
+                    else {
+                        valueOfAssignment.add(matcher.group(i));//else just assign the right value
+                    }
+            }
+        }
+
+        for(int i = 0; i<variableName.size();i++){
+            pairsOfAssignment.put(variableName.get(i),valueOfAssignment.get(i));
+        }
+        /*System.out.println("Assignments: " + assignments);
+        System.out.println("Name of variable: " + variableName);
+        System.out.println("Values of assignments: " + valueOfAssignment);
+        System.out.println("Assignmentpairs: " + pairsOfAssignment);*/
+
+        String regex2 = "[p][r][i][n][t][(]([a-z][a-z]*)[)]";
+        final Pattern pattern2 = Pattern.compile(regex2, Pattern.MULTILINE);
+        final Matcher matcher2 = pattern2.matcher(code);
+        while (matcher2.find()) {
+            //System.out.println(matcher2.group(0));
+            //System.out.println(matcher2.group(1));
+            printStatements.add(matcher2.group(1));
+        }
+        for(int i = 0; i<printStatements.size();i++){
+            for(String name: pairsOfAssignment.keySet())
+                if(printStatements.get(i).equals(name)){
+                    output.add(pairsOfAssignment.get(name));
+                }
         }//System.out.println(output);
 
         return output;
     }
     public static void main(String[] args ){
-        //helpermethod();
+        helpermethod();
     }
 }
--- Task2/Task2/src/TestInterpreterMilestone1.java
+++ Task2/Task2/src/TestInterpreterMilestone1.java
@@ -9,7 +9,7 @@ public class TestInterpreterMilestone1 {
         String code = "var foo; foo = 23; print(foo);";
         TinyPLParser.ProgramContext tree = Util.parseCode(code);
         InterpreterMilestone1 interpreter = new InterpreterMilestone1(tree);
-        List<String> output = interpreter.run();
+        List<String> output = interpreter.run(code);//wrong without code
         assertEquals(1, output.size());
         assertEquals("23", output.get(0));
     }
@@ -19,10 +19,24 @@ public class TestInterpreterMilestone1 {
         String code = "var x; var y; var z; z = 567; print(z); y = z; print(y);";
         TinyPLParser.ProgramContext tree = Util.parseCode(code);
         InterpreterMilestone1 interpreter = new InterpreterMilestone1(tree);
-        List<String> output = interpreter.run();
+        List<String> output = interpreter.run(code);//wrong without code
         assertEquals(2, output.size());
         assertEquals("567", output.get(0));
         assertEquals("567", output.get(1));
+    }
+    @Test
+    public void testProgram3() {
+        String code = "var x; var y; var z; var q; var v; var x; z = 567; x = z; y = 323; q = 456; v = 45; print(x); print(y); print(z); print(q); print(v); print(x);";
+        TinyPLParser.ProgramContext tree = Util.parseCode(code);
+        InterpreterMilestone1 interpreter = new InterpreterMilestone1(tree);
+        List<String> output = interpreter.run(code);//wrong without code
+        assertEquals(6, output.size());
+        assertEquals("567", output.get(0));
+        assertEquals("323", output.get(1));
+        assertEquals("567", output.get(2));
+        assertEquals("456", output.get(3));
+        assertEquals("45",  output.get(4));
+        assertEquals("567", output.get(5));
     }
 
 }
