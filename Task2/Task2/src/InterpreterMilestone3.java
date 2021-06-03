912611a569f7fe568d75e86f8c9bc60794077803
Daniel <danielweissenborn@outlook.de>
2021-06-03 17:35:09 +0200

    1 funktioniert jetzt komplett richtig OMG

--- Task2/Task2/src/InterpreterMilestone3.java
+++ Task2/Task2/src/InterpreterMilestone3.java
@@ -12,71 +12,9 @@ public class InterpreterMilestone3 {
         this.tree = tree;
     }
 
-    public List<String> run(String code) {
+    public List<String> run() {
         //public List<String> run() {//without parameter
         List<String> output = new ArrayList<>();
-
-        String regex = "[a-z][a-z]*\\s=\\s([a-z][a-z]*|[0-9][0-9]*);";
-
-        //String code = tree.toStringTree();
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        final Matcher matcher = pattern.matcher(code);
-
-        List<String> assignments = new ArrayList<>();
-        List<String> valueOfAssignment = new ArrayList<>();
-        List<String> variableName = new ArrayList<>();
-        List<String> printStatements = new ArrayList<>();
-        HashMap<String,String> pairsOfAssignment = new HashMap<>();
-
-        while (matcher.find()) {
-            System.out.println(matcher.group(0));
-            System.out.println(matcher.group(1));
-            assignments.add(matcher.group(0));
-            for (int i = 1; i <= matcher.groupCount(); i++) {
-
-                String firstWord = matcher.group(0).replaceAll(" .*", "");
-                variableName.add(firstWord);
-                System.out.println(firstWord);
-            }
-            if (!variableName.contains(matcher.group(1))) {
-                valueOfAssignment.add(matcher.group(1));//else just assign the right value
-                System.out.println("Value of assignment " + ": " + matcher.group(1));
-            } else {
-                for (int j = variableName.size()-2; j >= 0; j--) {
-                    //if the variable is already in the assignment list
-                    valueOfAssignment.add(valueOfAssignment.get(j));//somehow need value of this variable===)?
-                    System.out.println("Value of assignment " + ": " + valueOfAssignment.get(j));
-                    break;
-                    //break;
-                }
-            }
-        }
-
-
-
-        for(int i = 0; i<variableName.size();i++){
-            pairsOfAssignment.put(variableName.get(i),valueOfAssignment.get(i));
-        }
-        System.out.println("Assignments: " + assignments);
-        System.out.println("Name of variable: " + variableName);
-        System.out.println("Values of assignments: " + valueOfAssignment);
-        System.out.println("Assignmentpairs: " + pairsOfAssignment);
-
-        String regex2 = "[p][r][i][n][t][(]([a-z][a-z]*)[)]";
-        final Pattern pattern2 = Pattern.compile(regex2, Pattern.MULTILINE);
-        final Matcher matcher2 = pattern2.matcher(code);
-        while (matcher2.find()) {
-            System.out.println(matcher2.group(0));
-            System.out.println(matcher2.group(1));
-            printStatements.add(matcher2.group(1));
-        }
-        for(int i = 0; i<printStatements.size();i++){
-            for(String name: pairsOfAssignment.keySet())
-                if(printStatements.get(i).equals(name)){
-                    output.add(pairsOfAssignment.get(name));
-                }
-        }System.out.println(output);
-
         return output;
 
     }
--- Task2/Task2/src/TestInterpreterMilestone1.java
+++ Task2/Task2/src/TestInterpreterMilestone1.java
@@ -1,27 +1,26 @@
 import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
 import java.util.List;
 import org.junit.Test;
 
 public class TestInterpreterMilestone1 {
 
     @Test
-    public void testProgram1() {
+    public void testProgram1(){
         String code = "var foo; foo = 23; print(foo);";
         TinyPLParser.ProgramContext tree = Util.parseCode(code);
         InterpreterMilestone1 interpreter = new InterpreterMilestone1(tree);
-        List<String> output = interpreter.run(code);//wrong without code
-        //List<String> output = interpreter.run();
+        List<String> output = interpreter.run();//wrong without code
         assertEquals(1, output.size());
         assertEquals("23", output.get(0));
     }
-
     @Test
     public void testProgram2() {
         String code = "var x; var y; var z; z = 567; print(z); y = z; print(y);";
         TinyPLParser.ProgramContext tree = Util.parseCode(code);
         InterpreterMilestone1 interpreter = new InterpreterMilestone1(tree);
-        List<String> output = interpreter.run(code);//wrong without code
-        //List<String> output = interpreter.run();
+        List<String> output = interpreter.run();
         assertEquals(2, output.size());
         assertEquals("567", output.get(0));
         assertEquals("567", output.get(1));
@@ -31,8 +30,7 @@ public class TestInterpreterMilestone1 {
         String code = "var x; var y; var z; z = 567; x = z; y = 323; print(x); print(y); print(z);";
         TinyPLParser.ProgramContext tree = Util.parseCode(code);
         InterpreterMilestone1 interpreter = new InterpreterMilestone1(tree);
-        List<String> output = interpreter.run(code);//wrong without code
-        //List<String> output = interpreter.run();
+        List<String> output = interpreter.run();
         assertEquals(3, output.size());
         assertEquals("567", output.get(0));
         assertEquals("323", output.get(1));
@@ -43,8 +41,7 @@ public class TestInterpreterMilestone1 {
         String code = "var x; var y; var z; var q; z = 567; x = z; y = 323; q = z; q = y; print(x); print(y); print(z); print(q);";
         TinyPLParser.ProgramContext tree = Util.parseCode(code);
         InterpreterMilestone1 interpreter = new InterpreterMilestone1(tree);
-        List<String> output = interpreter.run(code);//wrong without code
-        //List<String> output = interpreter.run();
+        List<String> output = interpreter.run();
         assertEquals(4, output.size());
         assertEquals("567", output.get(0));
         assertEquals("323", output.get(1));
@@ -56,8 +53,7 @@ public class TestInterpreterMilestone1 {
         String code = "var x; var y; var z; var q; var o; z = 123; x = z; y = 2; q = z; q = y; o = 3; q = o; print(o); print(x); print(y); print(z); print(q);";
         TinyPLParser.ProgramContext tree = Util.parseCode(code);
         InterpreterMilestone1 interpreter = new InterpreterMilestone1(tree);
-        List<String> output = interpreter.run(code);//wrong without code
-        //List<String> output = interpreter.run();
+        List<String> output = interpreter.run();
         assertEquals(5, output.size());
         assertEquals("3", output.get(0));
         assertEquals("123", output.get(1));
@@ -70,8 +66,7 @@ public class TestInterpreterMilestone1 {
         String code = "var x; var y; var z; var q; var o; z = 123; x = z; y = z; q = z; o = z; print(o); print(x); print(y); print(z); print(q);";
         TinyPLParser.ProgramContext tree = Util.parseCode(code);
         InterpreterMilestone1 interpreter = new InterpreterMilestone1(tree);
-        List<String> output = interpreter.run(code);//wrong without code
-        //List<String> output = interpreter.run();
+        List<String> output = interpreter.run();
         assertEquals(5, output.size());
         assertEquals("123", output.get(0));
         assertEquals("123", output.get(1));
@@ -84,8 +79,7 @@ public class TestInterpreterMilestone1 {
         String code = "var x; var y; var z; var q; var o; z = 123; o = 2; print(o); x = z; y = z; q = z; print(x); print(y); print(z); print(q);";
         TinyPLParser.ProgramContext tree = Util.parseCode(code);
         InterpreterMilestone1 interpreter = new InterpreterMilestone1(tree);
-        List<String> output = interpreter.run(code);//wrong without code
-        //List<String> output = interpreter.run();
+        List<String> output = interpreter.run();
         assertEquals(5, output.size());
         assertEquals("2", output.get(0));
         assertEquals("123", output.get(1));
